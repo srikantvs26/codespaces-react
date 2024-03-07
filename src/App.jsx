116300f6e4918b107e4c464bdce43925ad24b831
srikant <srikantvs26@gmail.com>
2024-03-08 04:31:03 +0530

    basic layout ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,44 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
-
+import profilePic from './assets/profilePic.jpg';
 
 function App() {
   return (
     <>
-      <header>
-        <div className="container">
-          <div className='py-3 clearfix'>
-            <h1 className='site-title'>Bosch</h1>
-            <div className='site-nav'>
-              <ul class="nav justify-content-end">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Linked In</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Instagram</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Github</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </header>
       <main>
-        <div className='container'>
-          <div className='cover'>
-            <div className="cover-bg">
-              <div class="row">
-                <div class="col-lg-3 p-3 p-xl-4">
-                <div class="avatar hover-effect  bg-white  skills-section">
-                  <img src='Octocat.png' alt='image' width={200} height={200}/>
+        <div className="container-lg">
+          <div className="cover-page">
+            <div className="cover-bg p-4 p-lg-5">
+              <div className="row">
+                <div className="col-lg-4 col-md-5">
+                  <div className="avatar p-xxl-1">
+                    <img
+                      src={profilePic}
+                      alt="profile picture"
+                      height="200"
+                      width="200"
+                    />
                   </div>
                 </div>
-                <div class="col-lg-9">col-sm-8</div>
+                <div className="col-lg-8 col-md-7 text-white text-md-start text-center">
+                  <h1 className="mt-3">Srikant V S</h1>
+                  <p>Senior Software Engineer</p>
+
+                  <button type="button" class="btn btn-light mt-3 me-3">
+                    Download CV
+                  </button>
+                  <button type="button" class="btn btn-info mt-3 text-white">
+                    Hire Me
+                  </button>
+                </div>
               </div>
             </div>
+            <h1 className="m-5">Rest of the page</h1>
           </div>
         </div>
       </main>
     </>
-
   );
 }
 
