77fbb7a8d8a71030daacb99737e9bce68f99d16e
srikant <srikantvs26@gmail.com>
2024-03-08 05:24:27 +0530

    added aos library

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,12 @@ function App() {
             <div className="cover-bg p-4 p-lg-5">
               <div className="row">
                 <div className="col-lg-4 col-md-5">
-                  <div className="avatar p-xxl-1">
+                  <div
+                    className="avatar p-xxl-1"
+                    data-aos="fade-up"
+                    data-aos-easing="ease-in-out"
+                    data-aos-once="true"
+                  >
                     <img
                       src={profilePic}
                       alt="profile picture"
@@ -22,19 +27,22 @@ function App() {
                   </div>
                 </div>
                 <div className="col-lg-8 col-md-7 text-white text-md-start text-center">
-                  <h1 className="mt-3">Srikant V S</h1>
-                  <p>Senior Software Engineer</p>
-
-                  <button type="button" class="btn btn-light mt-3 me-3">
-                    Download CV
-                  </button>
-                  <button type="button" class="btn btn-info mt-3 text-white">
-                    Hire Me
-                  </button>
+                  <h1 className="mt-3" data-aos="fade-left" data-aos-delay="0">
+                    Srikant V S
+                  </h1>
+                  <p data-aos="fade-left" data-aos-delay="100">Senior Software Engineer</p>
+                  <div data-aos="fade-left" data-aos-delay="200">
+                    <button type="button" class="btn btn-light mt-3 me-3">
+                      Download CV
+                    </button>
+                    <button type="button" class="btn btn-info mt-3 text-white">
+                      Hire Me
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-            <h1 className="m-5">Rest of the page</h1>
+            <h1 className="m-5">...</h1>
           </div>
         </div>
       </main>
