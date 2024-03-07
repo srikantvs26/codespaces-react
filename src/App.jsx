6be47d33a148047888b714ded254c8e57e31f8f9
Srikant V S <38664247+srikantvs26@users.noreply.github.com>
2024-03-07 10:11:18 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+    <>
+      <header>
+        <div className="container">
+          <div className='py-3 clearfix'>
+            <h1 className='site-title'>Bosch</h1>
+            <div className='site-nav'>
+              <ul class="nav justify-content-end">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Linked In</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Instagram</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Github</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </header>
-    </div>
+      <main>
+        <div className='container'>
+          <div className='cover'>
+            <div className="cover-bg">
+              <div class="row">
+                <div class="col-lg-3 p-3 p-xl-4">
+                <div class="avatar hover-effect  bg-white  skills-section">
+                  <img src='Octocat.png' alt='image' width={200} height={200}/>
+                  </div>
+                </div>
+                <div class="col-lg-9">col-sm-8</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+
   );
 }
 
