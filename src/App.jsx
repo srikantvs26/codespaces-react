8c9251c280169b2f7a0106e3d5e008b4a3e3e3ba
srikant <srikantvs26@gmail.com>
2024-03-10 20:30:12 +0530

    added about, skills and animations

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Header />
       <main>
         <div className="container">
-          <div className="shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+          <div className="shadow-lg p-3 mb-5 bg-white rounded">
             <div className="cover-bg p-4 p-lg-5">
               <div className="row">
                 <div className="col-lg-4 col-md-5">
@@ -70,11 +70,129 @@ function App() {
                 </div>
               </div>
             </div>
-            <h1 className="m-5">...</h1>
+            <div className="row m-5">
+              <div className="col-lg-6">
+                <h2 className="text-dark">About Me</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos,
+                  adipisci magni. Quam provident, ipsa repellendus corrupti
+                  tenetur dicta illo sint. Aliquam ad perferendis eveniet
+                  nesciunt tempore quae pariatur maiores eaque?
+                </p>
+              </div>
+              <div className="col-lg-6">
+                <div className="row">
+                  <div className="col-lg-9 offset-lg-3">
+                    <div className="table-responsive">
+                      <table className="table">
+                        <tbody>
+                          <tr>
+                            <th scope="row">Email</th>
+                            <td>
+                              <a
+                                href="mailto:srikantvs26@gmail.com"
+                                className="text-decoration-none"
+                              >
+                                srikantvs26@gmail.com
+                              </a>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">Phone</th>
+                            <td>
+                              <a
+                                href="tel:+919886652951"
+                                className="text-decoration-none"
+                              >
+                                +91-9886652951
+                              </a>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">Address</th>
+                            <td>#1/12 Victoria Road, Bangalore</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div className="row m-5">
+              <div className="col-lg-12 mb-3">
+                <h2>Professional Skills</h2>{' '}
+              </div>
+              <div className="col-lg-6">
+                <div className="mb-2">
+                  <span>Java</span>
+                  <div
+                    className="progress my-2"
+                    role="progressbar"
+                    aria-label="Success example"
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <div
+                      class="progress-bar bg-success"
+                      style={{ width: '80%' }}
+                      data-aos="zoom-in-right"
+                      data-aos-delay="100"
+                      data-aos-once="true"
+                    ></div>
+                  </div>
+                </div>
+
+                <div className="mb-2">
+                  <span>Spring Boot</span>
+                  <div
+                    className="progress  my-2"
+                    role="progressbar"
+                    aria-label="Success example"
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <div
+                      class="progress-bar bg-success"
+                      style={{ width: '90%' }}
+                      data-aos="zoom-in-right"
+                      data-aos-delay="200"
+                      data-aos-once="true"
+                    ></div>
+                  </div>
+                </div>
+
+                <div>
+                  <span>Microservices</span>
+                  <div
+                    className="progress  my-2"
+                    role="progressbar"
+                    aria-label="Success example"
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <div
+                      class="progress-bar bg-success"
+                      style={{ width: '85%' }}
+                      data-aos="zoom-in-right"
+                      data-aos-delay="300"
+                      data-aos-once="true"
+                    ></div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <h3>...</h3>
+              </div>
+            </div>
           </div>
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
